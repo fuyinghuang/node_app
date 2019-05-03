33e11c327d9bfe11e8213a327cb8a3d910e560e9
huangfuin1101 <huangfuin1101@yahoo.com.tw>
2019-05-03 17:29:44 +0100

    added home and about page

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const path = require('path');
+const public = path.join(__dirname, 'public')
+
+app.use(express.static(`${__dirname}/public`));
+app.use('/', express.static(public));
+
+app.get('/', function(req, res) {
+    res.render(path.join(public, 'index.html'));
+});
+
+app.get('/about-me', function(req, res) {
+    res.render(path.join(public, 'about.html'));
+});
+
+
+
+
+app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
