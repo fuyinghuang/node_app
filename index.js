925875b19cd12d1354d1c8163d4f3ac4e6e7582d
huangfuin1101 <huangfuin1101@yahoo.com.tw>
2019-05-07 10:27:37 +0100

    rewrote error handler

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/', express.static(public));
 
 app.get('/', function(req, res) {
-    
     res.sendFile(path.join(public, 'index.html'));
    
 });
@@ -19,26 +18,14 @@ app.get('/about-me', function(req, res) {
 
 // error handler for not existing page
 
-// app.get('*',function(req, res, next){
-//     next(new Error('The route does not exist'));    
-// });
+app.get('*', function(req, res, next) {
+    let err = new Error('The page does not exist!');
+    next(err);
+  });
 
-app.get('*', function(req, res, next){  
-    next(err);  
-})
-
-// app.use(function (err, req, res, next) {
-//     console.error('this error',err.stack);
-//     res.status(500).send('Something went wrong!!');
-//   });
-
-app.use((err, req, res, next) => {
-    if (err) {
-      console.log('this is error', err.error );
-      res.status(500).send('The route does not exist!');
-    }
-    next();
-});
-
+app.use(function(err, req, res, next) {
+      res.send(err.message); 
+  });
+  
 
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
